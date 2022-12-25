e6a008510c5b6ca7a173989390a44e6968c606d0
Dog-hacker <121367566+Dog-hacker@users.noreply.github.com>
2022-12-25 13:56:24 -0600

    I made a text base game
    Still a work in progress that is made from a begginer

---
+++ Text base game.py
@@ -0,0 +1,13 @@
+print("you have almost no money but you use  it all to start a new life on an island")
+print("you take a boat onto the large island and stumble on the beach")
+island_name=input("what will you call this island?"  )
+print (island_name + " thats a pretty cool name ")
+player_name=input("but what is your name?")
+print( player_name + " ,mmhhh,nice to meet you")
+food_shelter=input("you eventually realize that you need food and shelter,what do ypu want to do first,type food for food,and type shelter for shelter")
+if(food_shelter=="food"):
+	print("you walk over to a tree and climb it to get its coconuts,you have obtained 2 coconuts")
+
+if(food_shelter=="shelter"):
+	print("you grab some leaves and sticks to make a sheltet,its not very   nice at the moment but you can improve it later on")
+second_choice=input("now type food to obtain food,but if you typed food first type           shelter ")
